0b6ef5550ee003afefccd50ccaf724fd6564728a
AbdelrahmanElawady <abdoelawady125@gmail.com>
2022-08-10 14:02:42 +0200

    Implement setWorkload to append workloads to planned deployments

--- deployer/manager.go
+++ deployer/manager.go
@@ -1,6 +1,8 @@
 package deployer
 
 import (
+	"errors"
+
 	substratemanager "github.com/threefoldtech/grid3-go/substrate_manager"
 	"github.com/threefoldtech/zos/client"
 	"github.com/threefoldtech/zos/pkg/gridtypes"
@@ -20,6 +22,7 @@ type DeploymentManager interface {
 }
 
 type deploymentManager struct {
+	twinID             uint32
 	deploymentIDs      map[uint32]uint64
 	deployments        map[uint32]gridtypes.Deployment
 	plannedDeployments map[uint32]gridtypes.Deployment
@@ -28,9 +31,10 @@ type deploymentManager struct {
 	//connection field
 }
 
-func NewDeploymentManager(sub substratemanager.Manager, nodeClient *client.NodeClient) DeploymentManager {
+func NewDeploymentManager(twinID uint32, sub substratemanager.Manager, nodeClient *client.NodeClient) DeploymentManager {
 
 	return &deploymentManager{
+		twinID,
 		make(map[uint32]uint64),
 		make(map[uint32]gridtypes.Deployment),
 		make(map[uint32]gridtypes.Deployment),
@@ -47,10 +51,37 @@ func (d *deploymentManager) CancelAll() {
 // }
 func (d *deploymentManager) Commit() error {
 	// generate gridtypes.Deployment from plannedDeployments
-	sub.
+	return nil
 
 }
 
 func (d *deploymentManager) SetWorkload(nodeID uint32, workload gridtypes.Workload) error {
 	// move workload to planned deployments
+	if pdCopy, ok := d.plannedDeployments[nodeID]; ok {
+		for _, wl := range pdCopy.Workloads {
+			if wl.Name == workload.Name {
+				return errors.New("Workload names should be unique")
+			}
+		}
+		pdCopy.Workloads = append(pdCopy.Workloads, workload)
+		d.plannedDeployments[nodeID] = pdCopy
+
+	} else {
+		newDeployment := gridtypes.Deployment{
+			Version: 0,
+			TwinID:  d.twinID,
+			SignatureRequirement: gridtypes.SignatureRequirement{
+				WeightRequired: 1,
+				Requests: []gridtypes.SignatureRequest{
+					{
+						TwinID: d.twinID,
+						Weight: 1,
+					},
+				},
+			},
+			Workloads: []gridtypes.Workload{workload},
+		}
+		d.plannedDeployments[nodeID] = newDeployment
+	}
+	return nil
 }
