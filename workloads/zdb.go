78b467e01dffbfd85cf1f81d01afce93c14e56cb
alaamahmoud.1223@gmail.com <alaamahmoud.1223@gmail.com>
2022-08-11 11:52:31 +0200

    NewZDBFromWorkload

--- workloads/zdb.go
+++ workloads/zdb.go
@@ -1,6 +1,9 @@
 package workloads
 
 import (
+	"encoding/json"
+
+	"github.com/pkg/errors"
 	"github.com/threefoldtech/grid3-go/deployer"
 	"github.com/threefoldtech/zos/pkg/gridtypes"
 	"github.com/threefoldtech/zos/pkg/gridtypes/zos"
@@ -17,6 +20,32 @@ type ZDB struct {
 	IPs         []string
 	Port        uint32
 	Namespace   string
+}
+
+func NewZDBFromWorkload(wl *gridtypes.Workload, nodeId uint32) (ZDB, error) {
+	dataI, err := wl.WorkloadData()
+	if err != nil {
+		return ZDB{}, errors.Wrap(err, "failed to get workload data")
+	}
+	// TODO: check ok?
+	data := dataI.(*zos.ZDB)
+	var result zos.ZDBResult
+
+	if err := json.Unmarshal(wl.Result.Data, &result); err != nil {
+		return ZDB{}, errors.Wrap(err, "failed to get zdb result")
+	}
+	return ZDB{
+		NodeId:      nodeId,
+		Name:        wl.Name.String(),
+		Description: wl.Description,
+		Password:    data.Password,
+		Public:      data.Public,
+		Size:        int(data.Size / gridtypes.Gigabyte),
+		Mode:        data.Mode.String(),
+		IPs:         result.IPs,
+		Port:        uint32(result.Port),
+		Namespace:   result.Namespace,
+	}, nil
 }
 
 func (z *ZDB) Stage(manager deployer.DeploymentManager) error {
