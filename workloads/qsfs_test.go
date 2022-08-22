5eff09ba9ab5b9eca412246f86752cc29cd2d685
AbdelrahmanElawady <abdoelawady125@gmail.com>
2022-08-22 19:25:33 +0200

    Add qsfs tests

---
+++ workloads/qsfs_test.go
@@ -0,0 +1,89 @@
+package workloads
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	mock_deployer "github.com/threefoldtech/grid3-go/mocks"
+	"github.com/threefoldtech/zos/pkg/gridtypes"
+	"github.com/threefoldtech/zos/pkg/gridtypes/zos"
+)
+
+func TestQSFSStage(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	manager := mock_deployer.NewMockDeploymentManager(ctrl)
+
+	qsfs := QSFS{
+		Name:                 "test",
+		Description:          "test des",
+		Cache:                2048,
+		MinimalShards:        10,
+		ExpectedShards:       20,
+		RedundantGroups:      2,
+		RedundantNodes:       5,
+		MaxZDBDataDirSize:    10,
+		EncryptionAlgorithm:  "AES",
+		EncryptionKey:        "4d778ba3216e4da4231540c92a55f06157cabba802f9b68fb0f78375d2e825af",
+		CompressionAlgorithm: "snappy",
+		Metadata: Metadata{
+			Type:                "zdb",
+			Prefix:              "test",
+			EncryptionAlgorithm: "AES",
+			EncryptionKey:       "4d778ba3216e4da4231540c92a55f06157cabba802f9b68fb0f78375d2e825af",
+			Backends: Backends{
+				{Address: "1.1.1.1", Namespace: "test ns", Password: "password"},
+			},
+		},
+		Groups: Groups{{Backends{
+			{Address: "2.2.2.2", Namespace: "test ns2", Password: "password2"},
+		}}},
+	}
+	k, _ := hex.DecodeString("4d778ba3216e4da4231540c92a55f06157cabba802f9b68fb0f78375d2e825af")
+	qsfsWl := gridtypes.Workload{
+		Version:     0,
+		Name:        gridtypes.Name("test"),
+		Type:        zos.QuantumSafeFSType,
+		Description: "test des",
+		Data: gridtypes.MustMarshal(zos.QuantumSafeFS{
+			Cache: gridtypes.Unit(uint64(2048) * uint64(gridtypes.Megabyte)),
+			Config: zos.QuantumSafeFSConfig{
+				MinimalShards:     10,
+				ExpectedShards:    20,
+				RedundantGroups:   2,
+				RedundantNodes:    5,
+				MaxZDBDataDirSize: 10,
+				Encryption: zos.Encryption{
+					Algorithm: zos.EncryptionAlgorithm("AES"),
+					Key:       zos.EncryptionKey(k),
+				},
+				Meta: zos.QuantumSafeMeta{
+					Type: "zdb",
+					Config: zos.QuantumSafeConfig{
+						Prefix: "test",
+						Encryption: zos.Encryption{
+							Algorithm: zos.EncryptionAlgorithm("AES"),
+							Key:       zos.EncryptionKey(k),
+						},
+						Backends: []zos.ZdbBackend{
+							{Address: "1.1.1.1", Namespace: "test ns", Password: "password"},
+						},
+					},
+				},
+				Groups: []zos.ZdbGroup{{Backends: []zos.ZdbBackend{
+					{Address: "2.2.2.2", Namespace: "test ns2", Password: "password2"},
+				}}},
+				Compression: zos.QuantumCompression{
+					Algorithm: "snappy",
+				},
+			},
+		}),
+	}
+	wlMap := map[uint32][]gridtypes.Workload{}
+	wlMap[1] = append(wlMap[1], qsfsWl)
+	manager.EXPECT().SetWorkloads(gomock.Eq(wlMap)).Return(nil)
+	err := qsfs.Stage(manager, 1)
+	assert.NoError(t, err)
+}
