956237324126b8c61d43d14c435b0969cb2ea26a
AbdelrahmanElawady <abdoelawady125@gmail.com>
2022-08-22 18:34:43 +0200

    Add vm tests

---
+++ workloads/vm_test.go
@@ -0,0 +1,100 @@
+package workloads
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"net"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	mock_deployer "github.com/threefoldtech/grid3-go/mocks"
+	"github.com/threefoldtech/zos/pkg/gridtypes"
+	"github.com/threefoldtech/zos/pkg/gridtypes/zos"
+)
+
+func TestVMStage(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	manager := mock_deployer.NewMockDeploymentManager(ctrl)
+
+	vm := VM{
+		Name:          "test",
+		Flist:         "flist test",
+		FlistChecksum: "flist cs test",
+		PublicIP:      true,
+		PublicIP6:     false,
+		Planetary:     true,
+		Corex:         false,
+		IP:            "1.1.1.1",
+		Description:   "test des",
+		Cpu:           2,
+		Memory:        2048,
+		RootfsSize:    4096,
+		Entrypoint:    "entrypoint",
+		Mounts: []Mount{
+			{DiskName: "disk", MountPoint: "mount"},
+		},
+		Zlogs: []Zlog{
+			{Output: "output"},
+		},
+		EnvVars:     map[string]string{"var1": "val1"},
+		NetworkName: "test network",
+	}
+	pubIPWl := gridtypes.Workload{
+		Version: 0,
+		Name:    gridtypes.Name("testip"),
+		Type:    zos.PublicIPType,
+		Data: gridtypes.MustMarshal(zos.PublicIP{
+			V4: true,
+			V6: false,
+		}),
+	}
+	urlHash := md5.Sum([]byte("output"))
+	zlogWl := gridtypes.Workload{
+		Version: 0,
+		Name:    gridtypes.Name(hex.EncodeToString(urlHash[:])),
+		Type:    zos.ZLogsType,
+		Data: gridtypes.MustMarshal(zos.ZLogs{
+			ZMachine: gridtypes.Name("test"),
+			Output:   "output",
+		}),
+	}
+	vmWl := gridtypes.Workload{
+		Version: 0,
+		Name:    gridtypes.Name("test"),
+		Type:    zos.ZMachineType,
+		Data: gridtypes.MustMarshal(zos.ZMachine{
+			FList: "flist test",
+			Network: zos.MachineNetwork{
+				Interfaces: []zos.MachineInterface{
+					{
+						Network: gridtypes.Name("test network"),
+						IP:      net.ParseIP("1.1.1.1"),
+					},
+				},
+				PublicIP:  gridtypes.Name("testip"),
+				Planetary: true,
+			},
+			ComputeCapacity: zos.MachineCapacity{
+				CPU:    uint8(2),
+				Memory: gridtypes.Unit(uint(2048)) * gridtypes.Megabyte,
+			},
+			Size:       gridtypes.Unit(4096) * gridtypes.Megabyte,
+			Entrypoint: "entrypoint",
+			Corex:      false,
+			Mounts: []zos.MachineMount{
+				{Name: gridtypes.Name("disk"), Mountpoint: "mount"},
+			},
+			Env: map[string]string{"var1": "val1"},
+		}),
+		Description: "test des",
+	}
+	wlMap := map[uint32][]gridtypes.Workload{}
+	wlMap[1] = append(wlMap[1], pubIPWl)
+	wlMap[1] = append(wlMap[1], zlogWl)
+	wlMap[1] = append(wlMap[1], vmWl)
+	manager.EXPECT().SetWorkloads(gomock.Eq(wlMap)).Return(nil)
+	err := vm.Stage(manager, 1)
+	assert.NoError(t, err)
+}
