0407282f41a9150d5f34dc1e0ab994d5906a910f
AbdelrahmanElawady <60783742+AbdelrahmanElawady@users.noreply.github.com>
2022-08-23 13:51:41 +0200

    Development workloads tests (#29)
    * Add deployment manager mocks and disk test
    
    * Change test function name
    
    * Add zdb tests
    
    * Add gateway_fqdn tests
    
    * Add gateway_name tests
    
    * Add vm tests
    
    * Add qsfs tests

---
+++ workloads/disk_test.go
@@ -0,0 +1,37 @@
+package workloads
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	mock_deployer "github.com/threefoldtech/grid3-go/mocks"
+	"github.com/threefoldtech/zos/pkg/gridtypes"
+	"github.com/threefoldtech/zos/pkg/gridtypes/zos"
+)
+
+func TestDiskStage(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	disk := Disk{
+		Name:        "test",
+		Size:        100,
+		Description: "test des",
+	}
+	diskWl := gridtypes.Workload{
+		Name:        gridtypes.Name("test"),
+		Version:     0,
+		Type:        zos.ZMountType,
+		Description: "test des",
+		Data: gridtypes.MustMarshal(zos.ZMount{
+			Size: gridtypes.Unit(100) * gridtypes.Gigabyte,
+		}),
+	}
+	wlMap := map[uint32][]gridtypes.Workload{}
+	wlMap[1] = append(wlMap[1], diskWl)
+	manager := mock_deployer.NewMockDeploymentManager(ctrl)
+	manager.EXPECT().SetWorkloads(gomock.Eq(wlMap)).Return(nil)
+	err := disk.Stage(manager, 1)
+	assert.NoError(t, err)
+}
---
+++ workloads/zdb_test.go
@@ -0,0 +1,43 @@
+package workloads
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	mock_deployer "github.com/threefoldtech/grid3-go/mocks"
+	"github.com/threefoldtech/zos/pkg/gridtypes"
+	"github.com/threefoldtech/zos/pkg/gridtypes/zos"
+)
+
+func TestZDBStage(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	manager := mock_deployer.NewMockDeploymentManager(ctrl)
+
+	zdb := ZDB{
+		Name:        "test",
+		Password:    "password",
+		Public:      true,
+		Size:        100,
+		Description: "test des",
+		Mode:        "user",
+	}
+	zdbWl := gridtypes.Workload{
+		Name:        gridtypes.Name("test"),
+		Type:        zos.ZDBType,
+		Description: "test des",
+		Version:     0,
+		Data: gridtypes.MustMarshal(zos.ZDB{
+			Size:     gridtypes.Unit(100) * gridtypes.Gigabyte,
+			Mode:     zos.ZDBMode("user"),
+			Password: "password",
+			Public:   true,
+		}),
+	}
+	wlMap := map[uint32][]gridtypes.Workload{}
+	wlMap[1] = append(wlMap[1], zdbWl)
+	manager.EXPECT().SetWorkloads(gomock.Eq(wlMap)).Return(nil)
+	err := zdb.Stage(manager, 1)
+	assert.NoError(t, err)
+}
