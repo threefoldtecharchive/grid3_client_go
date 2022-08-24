5db3d04a0ff4a5d87876154066ee74f35b9a0daa
alaamahmoud.1223@gmail.com <alaamahmoud.1223@gmail.com>
2022-08-24 13:23:45 +0200

    add client_pool_mock && dev_substrate_mock

--- go.mod
+++ go.mod
@@ -8,9 +8,8 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.7.0
 	github.com/threefoldtech/go-rmb v0.2.0
-	github.com/threefoldtech/grid_proxy_server v1.5.6
 	github.com/threefoldtech/substrate-client v0.0.0-20220808155028-1d74b8477705
-	github.com/threefoldtech/zos v0.5.6-0.20220804142531-495bf966448a
+	github.com/threefoldtech/zos v0.5.6-0.20220629073825-73f495f28c80
 )
 
 require (
@@ -40,6 +39,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rs/cors v1.8.2 // indirect
 	github.com/rs/zerolog v1.26.0 // indirect
+	github.com/threefoldtech/grid_proxy_server v1.5.7
 	github.com/vedhavyas/go-subkey v1.0.3 // indirect
 	golang.org/x/crypto v0.0.0-20220518034528-6f7dac969898 // indirect
 	golang.org/x/sys v0.0.0-20220403020550-483a9cbc67c0 // indirect
--- tests/deployer_tests/deployer_test.go
+++ tests/deployer_tests/deployer_test.go
@@ -1,13 +1,19 @@
 package deployertests
 
 import (
+	"context"
 	"encoding/hex"
+	"encoding/json"
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/threefoldtech/grid3-go/deployer"
+	client "github.com/threefoldtech/grid3-go/node"
 	mock "github.com/threefoldtech/grid3-go/tests/mocks"
 	"github.com/threefoldtech/grid3-go/workloads"
 	"github.com/threefoldtech/substrate-client"
+
 	"github.com/threefoldtech/zos/pkg/gridtypes"
 	"github.com/threefoldtech/zos/pkg/gridtypes/zos"
 )
@@ -75,14 +81,87 @@ func TestCreate(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	gridClient := mock.NewMockClient(ctrl)
-	// cl := mock.NewRMBMockClient(ctrl)
-	// sub := mock.
+	cl := mock.NewRMBMockClient(ctrl)
+	sub := mock.NewMockSubstrateExt(ctrl)
+	ncPool := mock.NewMockNodeClientCollection(ctrl)
+	deployer := deployer.NewDeployer(
+		identity,
+		214,
+		gridClient,
+		ncPool,
+		true,
+	)
 
+	dl1, dl2 := deployment1(identity, true, 0), deployment2(identity)
+	newDls := map[uint32]gridtypes.Deployment{
+		10: dl1,
+		20: dl2,
+	}
+
+	dl1.ContractID = 100
+	dl2.ContractID = 200
+
+	sub.EXPECT().
+		CreateNodeContract(
+			identity,
+			uint32(10),
+			nil,
+			hash(&dl1),
+			uint32(0),
+			true,
+		).Return(uint64(100), nil)
+	sub.EXPECT().
+		CreateNodeContract(
+			identity,
+			uint32(20),
+			nil,
+			hash(&dl2),
+			uint32(0),
+			true,
+		).Return(uint64(200), nil)
+	ncPool.EXPECT().
+		GetNodeClient(sub, uint32(10)).
+		Return(client.NewNodeClient(13, cl), nil)
+	ncPool.EXPECT().
+		GetNodeClient(sub, uint32(20)).
+		Return(client.NewNodeClient(23, cl), nil)
+	cl.EXPECT().
+		Call(gomock.Any(), uint32(13), "zos.deployment.deploy", dl1, gomock.Any()).
+		DoAndReturn(func(ctx context.Context, twin uint32, fn string, data, result interface{}) error {
+			dl1.Workloads[0].Result.State = gridtypes.StateOk
+			dl1.Workloads[0].Result.Data, _ = json.Marshal(zos.GatewayProxyResult{})
+			return nil
+		})
+	cl.EXPECT().
+		Call(gomock.Any(), uint32(23), "zos.deployment.deploy", dl2, gomock.Any()).
+		DoAndReturn(func(ctx context.Context, twin uint32, fn string, data, result interface{}) error {
+			dl2.Workloads[0].Result.State = gridtypes.StateOk
+			dl2.Workloads[0].Result.Data, _ = json.Marshal(zos.GatewayFQDNResult{})
+			return nil
+		})
+	cl.EXPECT().
+		Call(gomock.Any(), uint32(13), "zos.deployment.get", gomock.Any(), gomock.Any()).
+		DoAndReturn(func(ctx context.Context, twin uint32, fn string, data, result interface{}) error {
+			var res *gridtypes.Deployment = result.(*gridtypes.Deployment)
+			*res = dl1
+			return nil
+		})
+	cl.EXPECT().
+		Call(gomock.Any(), uint32(23), "zos.deployment.get", gomock.Any(), gomock.Any()).
+		DoAndReturn(func(ctx context.Context, twin uint32, fn string, data, result interface{}) error {
+			var res *gridtypes.Deployment = result.(*gridtypes.Deployment)
+			*res = dl2
+			return nil
+		})
+	deployer.(*DeployerImpl).validator = &EmptyValidator{}
+	contracts, err := deployer.Deploy(context.Background(), sub, nil, newDls)
+	assert.NoError(t, err)
+	assert.Equal(t, contracts, map[uint32]uint64{10: 100, 20: 200})
 }
 
-func TestUpdate(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	gridClient := mock.NewMockClient(ctrl)
-	// cl := mock.
-}
+// func TestUpdate(t *testing.T) {
+// 	ctrl := gomock.NewController(t)
+// 	defer ctrl.Finish()
+// 	gridClient := mock.NewMockClient(ctrl)
+// 	// cl := mock.
+// }
