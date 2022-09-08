d5c24f1e9866ba78eb1be801da2a0dd2b0041540
alaamahmoud.1223@gmail.com <alaamahmoud.1223@gmail.com>
2022-09-08 14:36:34 +0200

    test public ipv4

--- tests/integration/two_vms_different_network_test.go
+++ tests/integration/two_vms_different_network_test.go
@@ -2,9 +2,7 @@ package integration
 
 import (
 	"context"
-	"fmt"
-
-	// "strings"
+	"strings"
 
 	"net"
 	"os"
@@ -75,127 +73,135 @@ func TestTwoVmDifferentNet(t *testing.T) {
 		PublicIP6:   true,
 	}
 
-	// t.Run("check public ipv6 and yggdrasil", func(t *testing.T) {
-
-	// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
-	// 	defer cancel()
-
-	// 	_, err := network1.Stage(ctx, apiClient)
-	// 	assert.NoError(t, err)
-
-	// 	_, err = network2.Stage(ctx, apiClient)
-	// 	assert.NoError(t, err)
-
-	// 	err = vm1.Stage(manager, 14)
-	// 	assert.NoError(t, err)
-
-	// 	err = vm2.Stage(manager, 14)
-	// 	assert.NoError(t, err)
-
-	// 	err = manager.Commit(ctx)
-	// 	assert.NoError(t, err)
-	// 	defer manager.CancelAll()
-
-	// 	res1, err := loader.LoadVmFromGrid(manager, 14, "vm1")
-	// 	assert.NoError(t, err)
-	// 	res2, err := loader.LoadVmFromGrid(manager, 14, "vm2")
-	// 	assert.NoError(t, err)
-	// 	defer manager.CancelAll()
-
-	// 	yggIP1 := res1.YggIP
-	// 	yggIP2 := res2.YggIP
-
-	// 	privateIP1 := res1.IP
-	// 	privateIP2 := res2.IP
-
-	// 	public1Ip6 := strings.Split(res1.ComputedIP6, "/")[0]
-	// 	public2Ip6 := strings.Split(res2.ComputedIP6, "/")[0]
-
-	// 	if !Wait(yggIP1, "22") {
-	// 		t.Errorf("yggdrasil IP 1 isn't reachable")
-	// 	}
-	// 	if !Wait(yggIP2, "22") {
-	// 		t.Errorf("yggdrasil IP 2 isn't reachable")
-	// 	}
-
-	// 	_, err = RemoteRun("root", yggIP1, "apt install -y netcat")
-	// 	assert.NoError(t, err)
-
-	// 	_, err = RemoteRun("root", yggIP2, "apt install -y netcat")
-	// 	assert.NoError(t, err)
-
-	// 	// check privateIP2 from vm1
-	// 	_, err = RemoteRun("root", yggIP2, "nc -z "+privateIP1+" 22")
-	// 	assert.NoError(t, err)
-
-	// 	// check privateIP1 from vm2
-	// 	_, err = RemoteRun("root", yggIP1, "nc -z "+privateIP2+" 22")
-	// 	assert.NoError(t, err)
-
-	// 	// check yggIP2 from vm1
-	// 	_, err = RemoteRun("root", yggIP1, "nc -z "+yggIP2+" 22")
-	// 	assert.NoError(t, err)
-
-	// 	// check yggIP1 from vm2
-	// 	_, err = RemoteRun("root", yggIP2, "nc -z "+yggIP1+" 22")
-	// 	assert.NoError(t, err)
-
-	// 	// check publicIP62 from vm1
-	// 	_, err = RemoteRun("root", yggIP1, "nc -z "+public2Ip6+" 22")
-	// 	assert.NoError(t, err)
-
-	// 	// // check publicIP61 from vm2
-	// 	_, err = RemoteRun("root", yggIP2, "nc -z "+public1Ip6+" 22")
-	// 	assert.NoError(t, err)
-	// })
-
-	t.Run("check public ipv4", func(t *testing.T) {
-		network1Tmp := network1
-		network2Tmp := network2
-		vm1Tmp := vm1
-		vm2Tmp := vm2
-
-		network1Tmp.Nodes = []uint32{45}
-		network2Tmp.Nodes = []uint32{45}
-		vm1Tmp.PublicIP = true
-		vm2Tmp.PublicIP = true
+	t.Run("check public ipv6 and yggdrasil", func(t *testing.T) {
 
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 		defer cancel()
 
-		_, err := network1Tmp.Stage(ctx, apiClient)
+		_, err := network1.Stage(ctx, apiClient)
 		assert.NoError(t, err)
 
-		_, err = network2Tmp.Stage(ctx, apiClient)
+		_, err = network2.Stage(ctx, apiClient)
 		assert.NoError(t, err)
 
-		err = manager.Commit(ctx)
-		assert.NoError(t, err)
-		defer manager.CancelAll()
-
-		err = vm1Tmp.Stage(manager, 45)
+		err = vm1.Stage(manager, 14)
 		assert.NoError(t, err)
 
-		err = vm2Tmp.Stage(manager, 45)
+		err = vm2.Stage(manager, 14)
 		assert.NoError(t, err)
 
 		err = manager.Commit(ctx)
 		assert.NoError(t, err)
 		defer manager.CancelAll()
 
-		if err != nil {
-			t.Error(err)
-			t.FailNow()
+		res1, err := loader.LoadVmFromGrid(manager, 14, "vm1")
+		assert.NoError(t, err)
+		res2, err := loader.LoadVmFromGrid(manager, 14, "vm2")
+		assert.NoError(t, err)
+		defer manager.CancelAll()
+
+		yggIP1 := res1.YggIP
+		yggIP2 := res2.YggIP
+
+		privateIP1 := res1.IP
+		privateIP2 := res2.IP
+
+		public1Ip6 := strings.Split(res1.ComputedIP6, "/")[0]
+		public2Ip6 := strings.Split(res2.ComputedIP6, "/")[0]
+
+		if !Wait(yggIP1, "22") {
+			t.Errorf("yggdrasil IP 1 isn't reachable")
+		}
+		if !Wait(yggIP2, "22") {
+			t.Errorf("yggdrasil IP 2 isn't reachable")
 		}
 
-		res1, err := loader.LoadVmFromGrid(manager, 45, "vm1")
+		_, err = RemoteRun("root", yggIP1, "apt install -y netcat")
 		assert.NoError(t, err)
-		res2, err := loader.LoadVmFromGrid(manager, 45, "vm2")
+
+		_, err = RemoteRun("root", yggIP2, "apt install -y netcat")
+		assert.NoError(t, err)
+
+		_, err = RemoteRun("root", yggIP2, "nc -z "+privateIP1+" 22")
+		assert.NoError(t, err)
+
+		_, err = RemoteRun("root", yggIP1, "nc -z "+privateIP2+" 22")
+		assert.NoError(t, err)
+
+		_, err = RemoteRun("root", yggIP1, "nc -z "+yggIP2+" 22")
+		assert.NoError(t, err)
+
+		_, err = RemoteRun("root", yggIP2, "nc -z "+yggIP1+" 22")
+		assert.NoError(t, err)
+
+		_, err = RemoteRun("root", yggIP1, "nc -z "+public2Ip6+" 22")
+		assert.NoError(t, err)
+
+		_, err = RemoteRun("root", yggIP2, "nc -z "+public1Ip6+" 22")
+		assert.NoError(t, err)
+	})
+
+	t.Run("check public ipv4", func(t *testing.T) {
+		network1.Nodes = []uint32{13}
+		network2.Nodes = []uint32{13}
+
+		vm1.PublicIP = true
+		vm2.PublicIP = true
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+		defer cancel()
+
+		_, err := network1.Stage(ctx, apiClient)
+		assert.NoError(t, err)
+
+		_, err = network2.Stage(ctx, apiClient)
+		assert.NoError(t, err)
+
+		err = manager.Commit(ctx)
 		assert.NoError(t, err)
 		defer manager.CancelAll()
 
-		fmt.Print(res1)
-		fmt.Println(res2)
+		err = vm1.Stage(manager, 13)
+		assert.NoError(t, err)
+
+		err = vm2.Stage(manager, 13)
+		assert.NoError(t, err)
+
+		err = manager.Commit(ctx)
+		assert.NoError(t, err)
+		defer manager.CancelAll()
+
+		res1, err := loader.LoadVmFromGrid(manager, 13, "vm1")
+		assert.NoError(t, err)
+		res2, err := loader.LoadVmFromGrid(manager, 13, "vm2")
+		assert.NoError(t, err)
+		defer manager.CancelAll()
+
+		yggIP1 := res1.YggIP
+		yggIP2 := res2.YggIP
+
+		publicIP1 := res1.ComputedIP
+		publicIP2 := res2.ComputedIP
+
+		if !Wait(yggIP1, "22") {
+			t.Errorf("yggdrasil IP 1 isn't reachable")
+		}
+
+		if !Wait(yggIP2, "22") {
+			t.Errorf("yggdrasil IP 2 isn't reachable")
+		}
+
+		_, err = RemoteRun("root", yggIP1, "apt install -y netcat")
+		assert.NoError(t, err)
+
+		_, err = RemoteRun("root", yggIP2, "apt install -y netcat")
+		assert.NoError(t, err)
+
+		_, err = RemoteRun("root", yggIP1, "nc -z "+publicIP2+" 22")
+		assert.NoError(t, err)
+
+		_, err = RemoteRun("root", yggIP2, "nc -z "+publicIP1+" 22")
+		assert.NoError(t, err)
 
 	})
 
